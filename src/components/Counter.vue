d4a4d25beaf1bc999d22d85ff5b7d4bf90891877
Josué Eliezer Gómez Soto <jsegomez06@gmail.com>
2023-05-29 22:23:15 -0600

    fundamentos de VUE

---
+++ src/components/Counter.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="text-center">
+    <h2>{{ customTitle }}</h2>
+    <p>{{ counter }}</p>
+    <button class="btn btn-primary" v-on:click="() => changeValue(1)">+1</button>
+    <button class="btn btn-primary mx-3" @click="() => changeValue(0)">reset</button>
+    <button class="btn btn-primary" @click="() => changeValue(-1)">-1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    start: Number
+  },
+  data() {
+    return {
+      counter: this.start,
+    };
+  },
+  methods: {
+    changeValue(valueFn) {
+      if (valueFn == 0) {
+        this.counter = 0;
+        return;
+      }
+      return this.counter = this.counter + valueFn;
+    },
+  },
+  computed() {
+    return {
+      customTitle(){        
+        return this.start || 'Counter'
+      }
+    };
+  },
+};
+</script>
+
+<style>
+</style>
+
+
+
+
